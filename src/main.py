c49a788105098edebd012c4656199ba569e81de3
hemukka <henri.specops@hotmail.com>
2025-04-13 15:11:02 +0300

    dir paths into variables, additional prints describing generation process, check that file has .md suffix before generating it

--- src/main.py
+++ src/main.py
@@ -5,23 +5,23 @@ from re import findall
 
 from markdown_html import markdown_to_html_node
 
-def copy_static_files(dir="static", cur_path=""):
-    lsd = os.listdir(dir)
-    #print(lsd)
 
-    for item in lsd:
-        src = os.path.join(dir, item)
-        dst = os.path.join("docs", cur_path, item)
+def copy_static_files(from_dir_path, dest_dir_path):
+    """ Copy everything from_dir_path to dest_dir_path. """
+    for item in os.listdir(from_dir_path):
+        src = os.path.join(from_dir_path, item)
+        dst = os.path.join(dest_dir_path, item)
         if os.path.isfile(src):
-            print(f"{src} -> {dst}")
+            print(f"Copying file: {src} -> {dst}")
             shutil.copy(src, dst)
         else:
-            os.mkdir(dst)
             #print(f"-> {src}")
-            copy_static_files(src, os.path.join(cur_path, item))
+            os.mkdir(dst)
+            copy_static_files(src, dst)
 
 
 def extract_title(markdown):
+    """ Return text of the first h1 level heading (#). """
     title = findall(r"(?m)^# (.*)", markdown)
     if len(title) == 0:
         raise Exception("no h1 header for title")
@@ -29,6 +29,7 @@ def extract_title(markdown):
 
 
 def generate_page(from_path, template_path, dest_path, basepath):
+    """ Convert markdown file in from_path to html file in dest_path. """
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path) as file:
         markdown = file.read()
@@ -52,29 +53,48 @@ def generate_page(from_path, template_path, dest_path, basepath):
     
 
 def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, basepath):
+    """ Call generate_content on all files in dir_path_content. """
     for item in os.listdir(dir_path_content):
         content_path = os.path.join(dir_path_content, item)
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(content_path):
-            generate_page(content_path, template_path, dest_path.replace(".md", ".html"), basepath)
+            if content_path.endswith(".md"):
+                generate_page(content_path,
+                              template_path,
+                              dest_path.replace(".md", ".html"),
+                              basepath)
+            else:
+                print(f"Skipping non-markdown file: {content_path}")
         else:
             os.mkdir(dest_path)
             generate_pages_recursively(content_path, template_path, dest_path, basepath)
 
+
+dir_path_static = "static"
+dir_path_public = "docs"
+dir_path_content = "content"
+template_path = "template.html"
+default_basepath = "/"
+
+
 def main():
     if len(sys.argv) < 2:
-        basepath = "/"
+        basepath = default_basepath
     else:
         basepath = sys.argv[1]
-        
+    print(f"Site's root path set as: {basepath}")
     
-    if os.path.exists("public"):
-        shutil.rmtree("public")
-    if os.path.exists("docs"):
-        shutil.rmtree("docs")
-    os.mkdir("docs")
-    copy_static_files()
-    generate_pages_recursively("content", "template.html", "docs", basepath)
+    print("Clearing public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+    os.mkdir(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_static_files(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursively(dir_path_content, template_path, dir_path_public, basepath)
+
 
 if __name__ == "__main__":
     main()
